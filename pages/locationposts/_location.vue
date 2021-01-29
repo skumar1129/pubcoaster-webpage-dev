54bd1ef1d2873cc30d2a7118969e781b602f1f26
Sonali Kumar <sonalikumar@0587352942.wireless.umich.net>
2021-01-29 12:54:08 -0500

    added post component

--- pages/locationposts/_location.vue
+++ pages/locationposts/_location.vue
@@ -1,9 +1,27 @@
 <template>
+  <div>
   <h1>{{this.$route.params.location}}</h1>
+  <v-spacer> </v-spacer>
+  <post v-bind:currentUser="String('helga')" v-bind:response="{
+      createdBy: 'helga',
+      picLink: 'testing',
+      bar: 'Cheers',
+      description: 'Litty mc titty',
+      rating: 10,
+      anonymous: false,
+      location: 'Chicago',
+      neighborhood: 'Niles',
+      comments: [{createdBy: 'helga', text: 'heyyy girl'}],
+      likes: 1
+  }"></post>
+  </div>
 </template>
 
 <script>
+import post from '~/components/post.vue';
+
 export default {
+  components: { post },
   name: "LocationPosts"
 }
 </script>
