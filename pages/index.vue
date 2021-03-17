ff76835e187bfd24408d310933962af21ed82973
Sahil <sahilkumar1129@gmail.com>
2021-03-17 14:28:05 -0400

    Add splash screen

--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,51 @@
 <template>
-  <div>
-    <h1><NuxtLink to="/home">Home Screen Vibes Homies</NuxtLink></h1>
-    <button @click="goToLocationPost">Go to location posts</button>
-  </div>
+  <v-app>
+    <img src="../assets/splash_screen.jpg" alt="Splash Screen IMG">
+  </v-app>
 </template>
 
 <script>
-import { ref, defineComponent} from '@nuxtjs/composition-api';
-
+import { onMounted ,computed, defineComponent } from '@nuxtjs/composition-api';
 export default defineComponent ({
+  // TODO: Change this along with the firebase calls
   name: "Index",
-  setup(props, context) {
-    function goToLocationPost() {
-      this.$router.push({path: '/locationPosts/Chicago'})
-    }
-    return { goToLocationPost };
-  }
-
+  // setup() {
+  //   const authUser = computed(() => {
+  //     return $nuxt.$store.getters.user;
+  //   });
+  //   onMounted(() => {
+  //     if (!authUser.value) {
+  //       $nuxt.$router.push('/signin');
+  //     }
+  //     else if (!authUser.value.emailVerified) {
+  //       $nuxt.$router.push('/verifyemail');
+  //     }
+  //     else if (!authUser.value.displayName) {
+  //       $nuxt.$router.push('/adduserinfo');
+  //     }
+  //     else {
+  //       $nuxt.$router.push('/home');
+  //     }
+  //   })
+  // }
+  // fetch() {
+  //   const authUser = $nuxt.$store.getters.user;
+  //     if (!authUser) {
+  //       $nuxt.$router.push('/signin');
+  //     }
+  //     else if (!authUser.emailVerified) {
+  //       $nuxt.$router.push('/verifyemail');
+  //     }
+  //     else if (!authUser.displayName) {
+  //       $nuxt.$router.push('/adduserinfo');
+  //     }
+  //     else {
+  //       $nuxt.$router.push('/home');
+  //     }
+  // },
+  // fetchOnServer: false
 });
 </script>
 
 <style>
-
 </style>
