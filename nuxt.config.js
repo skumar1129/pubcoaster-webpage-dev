4b0da2bdf880a572cc5fea04b0bd596b1e5cc250
Sahil <sahilkumar1129@gmail.com>
2021-05-03 21:29:20 -0400

    Add fav icon

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,10 +9,17 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'msapplication-TileColor', content: '#da532c' },
+      { name: 'theme-color', content: '#ffffff' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'icon', type: 'image/png', size: '16x16', href: '/favicon-16x16.png' },
+      { rel: 'icon', type: 'image/png', size: '32x32', href: '/favicon-32x32.png' },
+      { rel: 'apple-touch-icon',  size: '180x180', href: '/apple-touch-icon.png' },
+      { rel: 'manifest', href: '/site.webmanifest' },
+      { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#5bbad5' }
     ]
   },
   // TODO: change to server for build
